fbf54ed1a21a9e4ef09f5a128d28772ab7155ed0
Ai-Lin Liou <alincode@gmail.com>
2019-02-01 23:29:02 +0800

    adjust for resolverEngine

--- src/getCompile.js
+++ src/getCompile.js
@@ -12,7 +12,7 @@ function getCompile(oldSolc) {
     compile = async function (sourcecode = '', getImportContent) {
       if (solcImport.isExistImport(sourcecode)) {
         if (getImportContent == undefined) {
-          getImportContent = solcResolver.getImportContent;
+          getImportContent = getContent();
         } else if (typeof getImportContent !== 'function') {
           throw Error('getContent should be a funcion.');
         }
@@ -26,4 +26,27 @@ function getCompile(oldSolc) {
     };
   });
   return compile;
+}
+
+function getContent() {
+  const ResolverEngine = require('solc-resolver').resolverEngine;
+  let resolverEngine = new ResolverEngine();
+
+  let resolveGithub = require('resolve-github');
+  resolverEngine.addResolver(resolveGithub);
+
+  let resolveHttp = require('resolve-http');
+  resolverEngine.addResolver(resolveHttp);
+
+  let resolveIPFS = require('resolve-ipfs');
+  resolverEngine.addResolver(resolveIPFS);
+
+  let resolveSwarm = require('resolve-swarm');
+  resolverEngine.addResolver(resolveSwarm);
+
+  const getImportContent = async function (path) {
+    return await resolverEngine.require(path);
+  };
+
+  return getImportContent;
 }
